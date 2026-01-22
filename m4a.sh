95fe76701ecbd7d7a7d7cfebe7bec3d1e5d780b9
steve-berlin <brainasaservice@gmail.com>
2026-01-22 18:23:31 +0100

    Fixed error message bug

--- m4a.sh
+++ m4a.sh
@@ -1,3 +1,2 @@
-echo "Provide valid YouTube link: "
 read -r link
 yt-dlp -U --update-to nightly --abort-on-error --verbose -x --audio-format m4a $link
--- webm.sh
+++ webm.sh
@@ -1,4 +1,3 @@
-source venv/bin/activate
 echo "Provide valid YouTube link: "
 read -r link
 yt-dlp -U --update-to nightly --abort-on-error --verbose $link
